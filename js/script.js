7a323cc59255c32bc22c02b56943b617b6a92261
Lautaro Luna <lautaroluna906@gmail.com>
2024-03-16 16:02:29 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+import $ from 'jquery';
+
+const card = document.getElementById('tajeta');
+
+card.addEventListener.addEventListener("mouseover", function() {
+    
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var myCarousel = new bootstrap.Carousel(document.getElementById('carrusel'), {
+      interval: 1000, // Cambia el intervalo de transición en milisegundos (opcional)
+      wrap: true // Si deseas que el carrusel vuelva al principio después de llegar al final (opcional)
+    });
+  });
+  
